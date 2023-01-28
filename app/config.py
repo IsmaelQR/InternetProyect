2fcb362bf3994312582da9b67b4c7acda115d332
angelbonillago <angel15b98g@gmail.com>
2023-01-27 23:55:31 -0500

    Config ok

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 class Config:
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'esta-es-una-clave-secreta'
-    SQLALCHEMY_DATABASE_URI="mysql://root@localhost/bdosc_api_Rest"
+    SQLALCHEMY_DATABASE_URI= os.environ.get('SQLALCHEMY_DATABASE_URI') 
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 
